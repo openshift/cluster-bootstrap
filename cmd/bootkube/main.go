04848d13e32ac7f180582a299d275b25b281ea57
Aaron Levy <aaronjlevy@gmail.com>
2016-05-27 13:48:33 -0700

    Add release build process

--- cmd/bootkube/main.go
+++ cmd/bootkube/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"github.com/coreos/bootkube/pkg/version"
 )
 
 var (
@@ -13,9 +15,20 @@ var (
 		Short: "Bootkube!",
 		Long:  "",
 	}
+
+	cmdVersion = &cobra.Command{
+		Use:   "version",
+		Short: "Output version information",
+		Long:  "",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Printf("Version: %s\n", version.Version)
+			return nil
+		},
+	}
 )
 
 func main() {
+	cmdRoot.AddCommand(cmdVersion)
 	if err := cmdRoot.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
