Arvinhub/bootkube

self hosted etcd migration

pkg/asset/asset.go
@@ -34,6 +34,8 @@ const (
 	AssetPathKubeDNSDeployment       = "manifests/kube-dns-deployment.yaml"
 	AssetPathKubeDNSSvc              = "manifests/kube-dns-svc.yaml"
 	AssetPathSystemNamespace         = "manifests/kube-system-ns.yaml"
+	AssetPathEtcdOperator            = "manifests/etcd-operator.yaml"
+	AssetPathEtcdSvc                 = "manifests/etcd-service.yaml"
 )
 
 // AssetConfig holds all configuration needed when generating
@@ -45,6 +47,7 @@ type Config struct {
 	CAPrivKey       *rsa.PrivateKey
 	AltNames        *tlsutil.AltNames
 	SelfHostKubelet bool
+	EtcdStorageType string
 }
 
 // NewDefaultAssets returns a list of default assets, optionally

pkg/asset/internal/templates.go
@@ -133,6 +133,7 @@ spec:
         - --insecure-port=8080
         - --advertise-address=$(MY_POD_IP)
         - --etcd-servers={{ range $i, $e := .EtcdServers }}{{ if $i }},{{end}}{{ $e }}{{end}}
+        - --storage-backend={{.EtcdStorageType}}
         - --allow-privileged=true
         - --service-cluster-ip-range=10.3.0.0/24
         - --admission-control=NamespaceLifecycle,LimitRanger,ServiceAccount,ResourceQuota
@@ -417,6 +418,44 @@ spec:
     protocol: UDP
   - name: dns-tcp
     port: 53
+    protocol: TCP
+`)
+	EtcdOperatorTemplate = []byte(`apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: etcd-operator
+  namespace: kube-system
+  labels:
+    k8s-app: etcd-operator
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        k8s-app: etcd-operator
+    spec:
+      containers:
+      - name: etcd-operator
+        image: quay.io/coreos/etcd-operator
+        env:
+        - name: MY_POD_NAMESPACE
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.namespace
+`)
+	EtcdSvcTemplate = []byte(`apiVersion: v1
+kind: Service
+metadata:
+  name: etcd-service
+  namespace: kube-system
+spec:
+  selector:
+    app: etcd
+    etcd_cluster: etcd-cluster
+  clusterIP: 10.3.0.20
+  ports:
+  - name: client
+    port: 2379
     protocol: TCP
 `)
 )

pkg/util/etcdutil/start_etcd.go
@@ -38,6 +38,8 @@ kind: Pod
 metadata:
   name: boot-etcd
   namespace: kube-system
+  labels:
+    k8s-app: boot-etcd
 spec:
   containers:
   - command:
@@ -61,4 +63,5 @@ spec:
     image: quay.io/coreos/etcd:v3.1.0-alpha.1
     name: etcd
   hostNetwork: true
+  restartPolicy: OnFailure
 `

openshift/cluster-bootstrap

pkg/asset/asset.go
@@ -34,6 +34,8 @@ const (
 	AssetPathKubeDNSDeployment       = "manifests/kube-dns-deployment.yaml"
 	AssetPathKubeDNSSvc              = "manifests/kube-dns-svc.yaml"
 	AssetPathSystemNamespace         = "manifests/kube-system-ns.yaml"
+	AssetPathEtcdOperator            = "manifests/etcd-operator.yaml"
+	AssetPathEtcdSvc                 = "manifests/etcd-service.yaml"
 )
 
 // AssetConfig holds all configuration needed when generating
@@ -45,6 +47,7 @@ type Config struct {
 	CAPrivKey       *rsa.PrivateKey
 	AltNames        *tlsutil.AltNames
 	SelfHostKubelet bool
+	StorageBackend  string
 }
 
 // NewDefaultAssets returns a list of default assets, optionally

pkg/asset/internal/templates.go
@@ -133,6 +133,7 @@ spec:
         - --insecure-port=8080
         - --advertise-address=$(MY_POD_IP)
         - --etcd-servers={{ range $i, $e := .EtcdServers }}{{ if $i }},{{end}}{{ $e }}{{end}}
+        - --storage-backend={{.StorageBackend}}
         - --allow-privileged=true
         - --service-cluster-ip-range=10.3.0.0/24
         - --admission-control=NamespaceLifecycle,LimitRanger,ServiceAccount,ResourceQuota
@@ -417,6 +418,44 @@ spec:
     protocol: UDP
   - name: dns-tcp
     port: 53
+    protocol: TCP
+`)
+	EtcdOperatorTemplate = []byte(`apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: etcd-operator
+  namespace: kube-system
+  labels:
+    k8s-app: etcd-operator
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        k8s-app: etcd-operator
+    spec:
+      containers:
+      - name: etcd-operator
+        image: quay.io/coreos/etcd-operator
+        env:
+        - name: MY_POD_NAMESPACE
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.namespace
+`)
+	EtcdSvcTemplate = []byte(`apiVersion: v1
+kind: Service
+metadata:
+  name: etcd-service
+  namespace: kube-system
+spec:
+  selector:
+    app: etcd
+    etcd_cluster: etcd-cluster
+  clusterIP: 10.3.0.15
+  ports:
+  - name: client
+    port: 2379
     protocol: TCP
 `)
 )

pkg/util/etcdutil/start_etcd.go
@@ -38,6 +38,8 @@ kind: Pod
 metadata:
   name: boot-etcd
   namespace: kube-system
+  labels:
+    k8s-app: boot-etcd
 spec:
   containers:
   - command:
@@ -61,4 +63,5 @@ spec:
     image: quay.io/coreos/etcd:v3.1.0-alpha.1
     name: etcd
   hostNetwork: true
+  restartPolicy: Never
 `