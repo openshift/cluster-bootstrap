bbbb7450c9823ab74d69884ded27cf7c62ab260a
Aaron Levy <aaronjlevy@gmail.com>
2017-05-19 14:53:35 -0700

    Bump Kubernetes to v1.6.4

--- cmd/bootkube/render.go
+++ cmd/bootkube/render.go
@@ -59,7 +59,7 @@ var (
 		Etcd:            "quay.io/coreos/etcd:v3.1.6",
 		EtcdOperator:    "quay.io/coreos/etcd-operator:v0.3.0",
 		Flannel:         "quay.io/coreos/flannel:v0.7.1-amd64",
-		Hyperkube:       "quay.io/coreos/hyperkube:v1.6.2_coreos.0",
+		Hyperkube:       "quay.io/coreos/hyperkube:v1.6.4_coreos.0",
 		Kenc:            "quay.io/coreos/kenc:48b6feceeee56c657ea9263f47b6ea091e8d3035",
 		KubeDNS:         "gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.1",
 		KubeDNSMasq:     "gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.1",
