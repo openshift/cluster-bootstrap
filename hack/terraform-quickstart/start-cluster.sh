5b3ed8ad8077d233b010ad4d2d083add3c0d8029
Patrick Baxter <pb@coreos.com>
2017-06-08 22:59:43 +0000

    terraform-quickstart: allow disabling cloud-provider integration

--- hack/terraform-quickstart/start-cluster.sh
+++ hack/terraform-quickstart/start-cluster.sh
@@ -6,7 +6,17 @@ export WORKER_IPS=`terraform output -json worker_ips | jq -r '.value[]'`
 export MASTER_IPS=`terraform output -json master_ips | jq -r '.value[]'`
 export SELF_HOST_ETCD=`terraform output self_host_etcd`
 export SSH_OPTS=${SSH_OPTS:-}" -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-export CLOUD_PROVIDER=aws
+export CLOUD_PROVIDER=${CLOUD_PROVIDER:-aws}
+
+# Normally we want to default to aws here since that is all terraform
+# supports and it is required for the e2e tests. However because of an
+# upstream bug, conformance tests won't pass with cloud provider integration
+# set to aws. So we need a knob to set the CLOUD_PROVIDER to nothing while
+# keeping aws as the default as to not mess up people using the e2e tests. 
+if [ "$CLOUD_PROVIDER" == "none" ] ; then
+	echo "cloud provider integration disabled"
+	CLOUD_PROVIDER=
+fi
 
 cd ../quickstart
 ./init-master.sh $BOOTSTRAP_IP
