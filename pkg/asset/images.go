aa6193addd811cc29348ecedd3526bfae6b1bd34
Diego Pontoriero <dpontor@gmail.com>
2017-07-11 10:40:49 -0700

    etcd-operator: v0.4.2

--- pkg/asset/images.go
+++ pkg/asset/images.go
@@ -3,7 +3,7 @@ package asset
 // DefaultImages are the defualt images bootkube components use.
 var DefaultImages = ImageVersions{
 	Etcd:            "quay.io/coreos/etcd:v3.1.8",
-	EtcdOperator:    "quay.io/coreos/etcd-operator:v0.4.0",
+	EtcdOperator:    "quay.io/coreos/etcd-operator:v0.4.2",
 	Flannel:         "quay.io/coreos/flannel:v0.7.1-amd64",
 	FlannelCNI:      "quay.io/coreos/flannel-cni:0.1.0",
 	Calico:          "quay.io/calico/node:v1.3.0",
