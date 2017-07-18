79823dc23d902d042cb1d1ff6ba164e807767f82
Diego Pontoriero <dpontor@gmail.com>
2017-07-18 12:24:29 -0700

    e2e: fix smoke test.
    It was using nested loops to create deployments and jobs, creating
    a bit of a mess.
    
    I also bumped the checkpoint waiting period to 60 seconds. Not ideal,
    but I'm seeing some instances where it takes >30 seconds to pivot back
    to the parent apiserver pod.

--- e2e/smoke_test.go
+++ e2e/smoke_test.go
@@ -3,41 +3,17 @@ package e2e
 import (
 	"testing"
 
-	"time"
-
 	"github.com/coreos/ktestutil/testworkload"
-	"k8s.io/apimachinery/pkg/util/wait"
-)
-
-const (
-	smokePingPollInterval = 1 * time.Second
-	smokePingPollTimeout  = 1 * time.Minute
 )
 
 func TestSmoke(t *testing.T) {
-	// 1. create nginx testworkload.
-	var nginx *testworkload.Nginx
-	if err := wait.Poll(networkPingPollInterval, networkPingPollTimeout, func() (bool, error) {
-		var err error
-		if nginx, err = testworkload.NewNginx(client, namespace, testworkload.WithNginxPingJobLabels(map[string]string{"allow": "access"})); err != nil {
-			t.Logf("failed to create test nginx: %v", err)
-			return false, nil
-		}
-		return true, nil
-	}); err != nil {
-		t.Fatalf("failed to create an testworkload: %v", err)
+	nginx, err := testworkload.NewNginx(client, namespace, testworkload.WithNginxPingJobLabels(map[string]string{"allow": "access"}))
+	if err != nil {
+		t.Fatalf("Test nginx creation failed: %v", err)
 	}
 	defer nginx.Delete()
 
-	// 2. Create a wget pod that hits the nginx.
-	if err := wait.Poll(smokePingPollInterval, smokePingPollTimeout, func() (bool, error) {
-		if err := nginx.IsReachable(); err != nil {
-			t.Logf("error not reachable %s: %v", nginx.Name, err)
-			return false, nil
-		}
-		return true, nil
-	}); err != nil {
-		t.Fatalf("couldn't reach the test workload %s: %v", nginx.Name, err)
+	if err := nginx.IsReachable(); err != nil {
+		t.Errorf("%s is not reachable: %v", nginx.Name, err)
 	}
-
 }
