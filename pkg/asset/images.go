4745460a84b925c1c73e07356ae3e839a68a05fb
Aaron Levy <aaronjlevy@gmail.com>
2017-09-01 16:13:56 -0700

    Bump k8s v1.7.5

--- pkg/asset/images.go
+++ pkg/asset/images.go
@@ -8,7 +8,7 @@ var DefaultImages = ImageVersions{
 	FlannelCNI:      "quay.io/coreos/flannel-cni:v0.2.0",
 	Calico:          "quay.io/calico/node:v2.4.0",
 	CalicoCNI:       "quay.io/calico/cni:v1.10.0",
-	Hyperkube:       "quay.io/coreos/hyperkube:v1.7.3_coreos.0",
+	Hyperkube:       "quay.io/coreos/hyperkube:v1.7.5_coreos.0",
 	Kenc:            "quay.io/coreos/kenc:0.0.2",
 	KubeDNS:         "gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.4",
 	KubeDNSMasq:     "gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.4",
