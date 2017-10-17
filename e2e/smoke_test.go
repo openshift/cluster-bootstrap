f3571489a967e017f8fdf9f4d91a425fe4701e78
Dalton Hubble <dghubble@gmail.com>
2017-10-17 13:42:52 -0700

    glide.yaml: Update ktestutil retry interval and timeout
    * Increase timeout allowed for the TestSmoke nginx workload

--- e2e/smoke_test.go
+++ e2e/smoke_test.go
@@ -2,13 +2,11 @@ package e2e
 
 import (
 	"testing"
-	"time"
 
 	"github.com/coreos/ktestutil/testworkload"
 )
 
 func TestSmoke(t *testing.T) {
-	testworkload.PollTimeoutForNginx = 5 * time.Minute
 	nginx, err := testworkload.NewNginx(client, namespace, testworkload.WithNginxPingJobLabels(map[string]string{"allow": "access"}))
 	if err != nil {
 		t.Fatalf("Test nginx creation failed: %v", err)
