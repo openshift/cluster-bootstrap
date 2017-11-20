2ac305fd29f45b15e39eac50b591c2a8a8849940
Dalton Hubble <dghubble@gmail.com>
2017-11-20 11:27:32 -0800

    pkg,hack: Update Kubernetes from v1.8.3 to v1.8.4

--- pkg/asset/images.go
+++ pkg/asset/images.go
@@ -8,7 +8,7 @@ var DefaultImages = ImageVersions{
 	FlannelCNI:      "quay.io/coreos/flannel-cni:v0.3.0",
 	Calico:          "quay.io/calico/node:v2.6.1",
 	CalicoCNI:       "quay.io/calico/cni:v1.11.0",
-	Hyperkube:       "gcr.io/google_containers/hyperkube:v1.8.3",
+	Hyperkube:       "gcr.io/google_containers/hyperkube:v1.8.4",
 	Kenc:            "quay.io/coreos/kenc:0.0.2",
 	KubeDNS:         "gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.5",
 	KubeDNSMasq:     "gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.5",
