1881bd7b9b8276b8d28be357c689c096e04c6e01
Ryan Phillips <ryan@trolocsis.com>
2018-04-12 17:15:15 -0500

    bump to 1.10.1 (#964)

--- hack/tests/conformance-test.sh
+++ hack/tests/conformance-test.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 CONFORMANCE_REPO=${CONFORMANCE_REPO:-github.com/kubernetes/kubernetes}
-CONFORMANCE_VERSION=${CONFORMANCE_VERSION:-v1.10.0}
+CONFORMANCE_VERSION=${CONFORMANCE_VERSION:-v1.10.1}
 
 usage() {
     echo "USAGE:"
