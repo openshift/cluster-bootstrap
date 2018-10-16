b43b578f1a3ea7b07dd61970429464a6e01cff94
Ryan Phillips <rphillips@redhat.com>
2018-10-16 12:13:24 -0500

    images: bump checkpointer for affinity fix

--- pkg/asset/images.go
+++ pkg/asset/images.go
@@ -9,5 +9,5 @@ var DefaultImages = ImageVersions{
 	CalicoCNI:       "quay.io/calico/cni:v2.0.0",
 	CoreDNS:         "k8s.gcr.io/coredns:1.1.3",
 	Hyperkube:       "k8s.gcr.io/hyperkube:v1.11.2",
-	PodCheckpointer: "quay.io/coreos/pod-checkpointer:9dc83e1ab3bc36ca25c9f7c18ddef1b91d4a0558",
+	PodCheckpointer: "quay.io/coreos/pod-checkpointer:018007e77ccd61e8e59b7e15d7fc5e318a5a2682",
 }
